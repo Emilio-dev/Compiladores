defa99afda8bfc9e7a07525ecbfc5b64eed1d331
Emilio Flores Castañeda <efloresc1602@alumno.ipn.mx>
2023-06-06 00:08:35 -0600

    fix a bug

--- analizadorSintactico.py
+++ analizadorSintactico.py
@@ -6,11 +6,10 @@ class Token:
         self.value = val
 
     def __str__(self):
-        return str(self.value)
+         return str(self.value)
 
     def __repr__(self):
         return str(self)
-
 
 class Lexer:
     def __init__(self, text):
@@ -21,7 +20,7 @@ class Lexer:
         self.column = 1
 
     def advance(self):
-
+        
         if self.current_char is not None and self.current_char == '\n':
             self.line += 1
             self.column = 1
@@ -36,7 +35,7 @@ class Lexer:
             if self.current_char.isspace():
                 self.advance()
                 continue
-
+            
             if self.current_char == '+':
                 self.advance()
                 return Token("masToken", '+')
@@ -54,7 +53,7 @@ class Lexer:
                 if self.current_char == '/':
                     self.skip_single_line_comment()
                     continue
-                elif self.current_char == '*':
+                elif self.current_char== '*':
                     self.skip_multi_line_comment()
                     continue
                 return Token("divToken", '/')
@@ -74,7 +73,7 @@ class Lexer:
             if self.current_char == ')':
                 self.advance()
                 return Token("rparenToken", ')')
-
+            
             if self.current_char == '{':
                 self.advance()
                 return Token("lcorchToken", '{')
@@ -82,128 +81,128 @@ class Lexer:
             if self.current_char == '}':
                 self.advance()
                 return Token("rcorchToken", '}')
-
+            
             if self.current_char == '=':
                 self.advance()
                 if self.current_char == '=':
-                    self.advance()
+                    self.advance()    
                     return Token("igualAToken", '==')
                 return Token("igualToken", '=')
-
+            
             if self.current_char == '!':
                 self.advance()
                 if self.current_char == '=':
-                    self.advance()
+                    self.advance()    
                     return Token("difereteToken", '!=')
-                return Token("notToken", '!')
+                return Token("notToken", '!')   
 
             if self.current_char == '>':
                 self.advance()
                 if self.current_char == '=':
-                    self.advance()
+                    self.advance()    
                     return Token("mayorIgualToken", '>=')
                 return Token("mayorToken", '>')
-
+                                 
             if self.current_char == '<':
                 self.advance()
                 if self.current_char == '=':
-                    self.advance()
+                    self.advance()    
                     return Token("menorIgualToken", '<=')
                 return Token("menorToken", '<')
 
             if self.current_char == ';':
                 self.advance()
                 return Token("pcomaToken", ';')
-
+                      
             if self.current_char == 'o':
                 self.advance()
-                return Token("orToken", "o")
-
+                return Token("orToken","o")
+            
             if self.current_char == 'y':
                 self.advance()
-                return Token("yToken", "y")
-
+                return Token("yToken","y")
+                       
             if self.current_char == '"':
                 return Token("stringToken", self.get_string())
-
+            
             if self.current_char.isdigit() or self.current_char == ".":
                 number_token = self.get_number()
-                return Token(number_token.type, number_token.value)
+                return Token(number_token.type,number_token.value)
 
             if self.current_char.isalpha():
                 identifier = self.get_identifier()
                 if identifier == "ademas":
-                    return Token("ademasToken", "ademas")
-
+                    return Token("ademasToken","ademas")
+                
                 elif identifier == "falso":
-                    return Token("falsoToken", "falso")
-
+                    return Token("falsoToken","falso")
+                
                 elif identifier == "para":
-                    return Token("paraToken", "para")
-
+                    return Token("paraToken","para")
+                
                 elif identifier == "fun":
-                    return Token("funToken", "fun")
-
+                    return Token("funToken","fun")
+                
                 elif identifier == "si":
-                    return Token("siToken", "si")
-
+                    return Token("siToken","si")
+                
                 elif identifier == "nulo":
-                    return Token("nuloToken", "nulo")
-
+                    return Token("nuloToken","nulo")
+                
                 elif identifier == "imprimir":
-                    return Token("imprimirToken", "imprimir")
-
+                    return Token("imprimirToken","imprimir")
+                
                 elif identifier == "retornar":
-                    return Token("retornarToken", "retornar")
-
+                    return Token("retornarToken","retornar")
+                
                 elif identifier == "super":
-                    return Token("superToken", "super")
-
+                    return Token("superToken","super")
+                
                 elif identifier == "entonces":
-                    return Token("entoncesToken", "entonces")
+                    return Token("entoncesToken","entonces")
 
                 elif identifier == "este":
-                    return Token("entoncesToken", "este")
-
+                    return Token("entoncesToken","este")
+                
                 elif identifier == "verdadero":
-                    return Token("verdaderoToken", "verdadero")
-
+                    return Token("verdaderoToken","verdadero")
+                
                 elif identifier == "var":
-                    return Token("varToken", "var")
-
+                    return Token("varToken","var")
+                               
                 elif identifier == "mientras":
-                    return Token("mientrasToken", "mientras")
-
+                    return Token("mientrasToken","mientras")
+                
                 elif identifier == "clase":
-                    return Token("claseToken", "clase")
-
+                    return Token("claseToken","clase")
+                
                 elif identifier == "para":
-                    return Token("paraToken", "para")
-
+                    return Token("paraToken","para")
+                
                 else:
                     return Token("idToken", identifier)
 
             self.error()
 
         return Token("EOF", None)
-
+    
     def skip_single_line_comment(self):
         while self.current_char is not None and self.current_char != '\n':
             self.advance()
         self.advance()  # Consume the newline character
 
     def skip_multi_line_comment(self):
-        self.advance()
-        self.advance()
+        self.advance()  
+        self.advance()  
         while self.current_char is not None:
-            if self.current_char == '*':
-                self.advance()
+            if self.current_char == '*':  
+                self.advance()  
                 if self.current_char == '/':
-                    self.advance()
+                    self.advance()  
                     return
             self.advance()
-        raise Exception('Unterminated multi-line comment')
-
+        raise Exception('Unterminated multi-line comment')   
+     
     def get_identifier(self):
         identifier = ''
         while self.current_char is not None and self.current_char.isalnum():
@@ -220,7 +219,7 @@ class Lexer:
             return Token("floatToken", float(result))
         else:
             return Token("integerToken", int(result))
-
+ 
     def get_string(self):
         self.advance()
         result = ''
@@ -234,8 +233,7 @@ class Lexer:
 
     def error(self):
         raise Exception("Carácter no válido en la línea {} y columna {}".format(self.line, self.column))
-
-
+           
 def tokenizar(texto):
     lexer = Lexer(texto)
     tokens = []
@@ -245,8 +243,7 @@ def tokenizar(texto):
             tokens.append(token)
             break
         tokens.append(token)
-    return tokens
-
+    return tokens 
 
 def PROGRAM(tokens):
     if len(tokens) == 0:
@@ -255,14 +252,13 @@ def PROGRAM(tokens):
     else:
         idx = 0
 
-        while idx < len(tokens) - 1:
-            if idx != -1 or tokens[idx].type != "EOF":
+        while idx < len(tokens)-1:
+            if idx!=-1 or tokens[idx].type != "EOF":
                 idx = DECLARATION(tokens, idx)
             else:
                 return -1
 
-
-def DECLARATION(tokens, idx):  # no se como poner expr_stmt
+def DECLARATION(tokens, idx):#no se como poner expr_stmt
     if idx >= len(tokens):
         return idx
 
@@ -273,13 +269,12 @@ def DECLARATION(tokens, idx):  # no se como poner expr_stmt
         idx = FUN_DECL(tokens, idx)
     elif tokens[idx].type == "varToken":
         idx = VAR_DECL(tokens, idx)
-    elif tokens[idx].type in ["paraToken", "siToken", "imprimirToken", "retornarToken", "mientrasToken", "lcorchToken"]:
+    elif tokens[idx].type in ["idToken","paraToken","siToken", "imprimirToken", "retornarToken", "mientrasToken", "lcorchToken"]:
         idx = STATEMENT(tokens, idx)
     else:
         print("Error: Invalid declaration at position " + str(idx))
         return -1
     return idx
-
 
 def CLASS_DECL(tokens, idx):
     if tokens[idx].type == "claseToken":
@@ -306,14 +301,12 @@ def CLASS_DECL(tokens, idx):
         return -1
     return idx
 
-
 def CLASS_INHER(tokens, idx):
     if tokens[idx].type == "menorToken":
         idx += 1
         if tokens[idx].type == "idToken":
             idx += 1
     return idx
-
 
 def FUN_DECL(tokens, idx):
     if tokens[idx].type == "funToken":
@@ -323,7 +316,6 @@ def FUN_DECL(tokens, idx):
         print("Error: 'fun' keyword expected at position " + str(idx))
         return -1
     return idx
-
 
 def VAR_DECL(tokens, idx):
     if tokens[idx].type == "varToken":
@@ -344,19 +336,17 @@ def VAR_DECL(tokens, idx):
         return -1
     return idx
 
-
 def VAR_INIT(tokens, idx):
     if tokens[idx].type == "igualToken":
         idx += 1
         idx = EXPRESSION(tokens, idx)
     return idx
 
-
 def STATEMENT(tokens, idx):
     if tokens[idx].type == "paraToken":
         idx = FOR_STMT(tokens, idx)
     elif tokens[idx].type == "siToken":
-        idx = IF_STMT(tokens, idx)
+        idx = IF_STMT(tokens, idx)    
     elif tokens[idx].type == "imprimirToken":
         idx = PRINT_STMT(tokens, idx)
     elif tokens[idx].type == "retornarToken":
@@ -369,7 +359,6 @@ def STATEMENT(tokens, idx):
         idx = EXPR_STMT(tokens, idx)
     return idx
 
-
 def EXPR_STMT(tokens, idx):
     idx = EXPRESSION(tokens, idx)
     if tokens[idx].type == "pcomaToken":
@@ -378,7 +367,6 @@ def EXPR_STMT(tokens, idx):
         print("Error: Missing ';' at position " + str(idx))
         return -1
     return idx
-
 
 def FOR_STMT(tokens, idx):
     if tokens[idx].type == "paraToken":
@@ -402,39 +390,35 @@ def FOR_STMT(tokens, idx):
         return -1
     return idx
 
-
-def FOR_STMT_1(tokens, idx):  # duda
+def FOR_STMT_1(tokens, idx): #duda
     if tokens[idx].type == "varToken":
-        idx = VAR_DECL(tokens, idx)
+        idx=VAR_DECL(tokens, idx)
         return idx
-    idx = EXPR_STMT(tokens, idx)
+    idx=EXPR_STMT(tokens, idx)
     if tokens[idx].type == "pcomaToken":
-        idx += 1
+            idx += 1
     else:
         print("Error: Missing ';' at position " + str(idx))
         return -1
     return idx
 
-
-def FOR_STMT_2(tokens, idx):  # no se bien si hay que declarar expression y luego checar ;
+def FOR_STMT_2(tokens, idx): #no se bien si hay que declarar expression y luego checar ;
     if tokens[idx].type == "pcomaToken":
         idx += 1
         return idx
     idx = EXPRESSION(tokens, idx)
     if tokens[idx].type == "pcomaToken":
-        idx += 1
+        idx += 1    
     else:
         print("Error: Missing ';' at position " + str(idx))
         return -1
     return idx
-
 
 def FOR_STMT_3(tokens, idx):
     if tokens[idx].type == "rparenToken":
         return idx
     idx = EXPRESSION(tokens, idx)
     return idx
-
 
 def IF_STMT(tokens, idx):
     if tokens[idx].type == "siToken":
@@ -457,13 +441,11 @@ def IF_STMT(tokens, idx):
         return -1
     return idx
 
-
 def ELSE_STATEMENT(tokens, idx):
     if tokens[idx].type == "entoncesToken":
         idx += 1
         idx = STATEMENT(tokens, idx)
     return idx
-
 
 def PRINT_STMT(tokens, idx):
     if tokens[idx].type == "imprimirToken":
@@ -479,7 +461,6 @@ def PRINT_STMT(tokens, idx):
         return -1
     return idx
 
-
 def RETURN_STMT(tokens, idx):
     if tokens[idx].type == "retornarToken":
         idx += 1
@@ -494,12 +475,10 @@ def RETURN_STMT(tokens, idx):
         return -1
     return idx
 
-
 def RETURN_EXP_OPC(tokens, idx):
     if tokens[idx].type != "pcomaToken":
         idx = EXPRESSION(tokens, idx)
     return idx
-
 
 def WHILE_STMT(tokens, idx):
     if tokens[idx].type == "mientrasToken":
@@ -521,7 +500,6 @@ def WHILE_STMT(tokens, idx):
         return -1
     return idx
 
-
 def BLOCK(tokens, idx):
     if tokens[idx].type == "lcorchToken":
         idx += 1
@@ -536,24 +514,20 @@ def BLOCK(tokens, idx):
         return -1
     return idx
 
-
 def BLOCK_DECL(tokens, idx):
     if tokens[idx].type != "rcorchToken":
         idx = DECLARATION(tokens, idx)
         idx = BLOCK_DECL(tokens, idx)
     return idx
 
-
 def EXPRESSION(tokens, idx):
     idx = ASSIGNMENT(tokens, idx)
     return idx
-
 
 def ASSIGNMENT(tokens, idx):
     idx = LOGIC_OR(tokens, idx)
     idx = ASSIGNMENT_OPC(tokens, idx)
     return idx
-
 
 def ASSIGNMENT_OPC(tokens, idx):
     if tokens[idx].type == "igualToken":
@@ -561,12 +535,10 @@ def ASSIGNMENT_OPC(tokens, idx):
         idx = EXPRESSION(tokens, idx)
     return idx
 
-
 def LOGIC_OR(tokens, idx):
     idx = LOGIC_AND(tokens, idx)
     idx = LOGIC_OR_2(tokens, idx)
     return idx
-
 
 def LOGIC_OR_2(tokens, idx):
     if tokens[idx].type == "orToken":
@@ -575,12 +547,10 @@ def LOGIC_OR_2(tokens, idx):
         idx = LOGIC_OR_2(tokens, idx)
     return idx
 
-
 def LOGIC_AND(tokens, idx):
     idx = EQUALITY(tokens, idx)
     idx = LOGIC_AND_2(tokens, idx)
     return idx
-
 
 def LOGIC_AND_2(tokens, idx):
     if tokens[idx].type == "andToken":
@@ -589,12 +559,10 @@ def LOGIC_AND_2(tokens, idx):
         idx = LOGIC_AND_2(tokens, idx)
     return idx
 
-
 def EQUALITY(tokens, idx):
     idx = COMPARISON(tokens, idx)
     idx = EQUALITY_2(tokens, idx)
     return idx
-
 
 def EQUALITY_2(tokens, idx):
     if tokens[idx].type in ["diferenteToken", "igualAToken"]:
@@ -603,12 +571,10 @@ def EQUALITY_2(tokens, idx):
         idx = EQUALITY_2(tokens, idx)
     return idx
 
-
 def COMPARISON(tokens, idx):
     idx = TERM(tokens, idx)
     idx = COMPARISON_2(tokens, idx)
     return idx
-
 
 def COMPARISON_2(tokens, idx):
     if tokens[idx].type in ["mayorToken", "mayorIgualToken", "menorToken", "menorIgualToken"]:
@@ -617,12 +583,10 @@ def COMPARISON_2(tokens, idx):
         idx = COMPARISON_2(tokens, idx)
     return idx
 
-
 def TERM(tokens, idx):
     idx = FACTOR(tokens, idx)
     idx = TERM_2(tokens, idx)
     return idx
-
 
 def TERM_2(tokens, idx):
     if tokens[idx].type in ["menosToken", "masToken"]:
@@ -631,12 +595,10 @@ def TERM_2(tokens, idx):
         idx = TERM_2(tokens, idx)
     return idx
 
-
 def FACTOR(tokens, idx):
     idx = UNARY(tokens, idx)
     idx = FACTOR_2(tokens, idx)
     return idx
-
 
 def FACTOR_2(tokens, idx):
     if tokens[idx].type in ["divToken", "multiToken"]:
@@ -644,7 +606,6 @@ def FACTOR_2(tokens, idx):
         idx = UNARY(tokens, idx)
         idx = FACTOR_2(tokens, idx)
     return idx
-
 
 def UNARY(tokens, idx):
     if tokens[idx].type in ["notToken", "menosToken"]:
@@ -654,14 +615,12 @@ def UNARY(tokens, idx):
         idx = CALL(tokens, idx)
     return idx
 
-
 def CALL(tokens, idx):
     idx = PRIMARY(tokens, idx)
     idx = CALL_2(tokens, idx)
     return idx
 
-
-def CALL_2(tokens, idx):  # creo faltan errores
+def CALL_2(tokens, idx): #creo faltan errores
     if tokens[idx].type == "lparenToken":
         idx += 1
         idx = ARGUMENTS_OPC(tokens, idx)
@@ -676,7 +635,6 @@ def CALL_2(tokens, idx):  # creo faltan errores
         idx = CALL_2(tokens, idx)
     return idx
 
-
 def CALL_OPC(tokens, idx):
     if tokens[idx].type == "puntoToken":
         idx += 1
@@ -688,10 +646,8 @@ def CALL_OPC(tokens, idx):
             return -1
     return idx
 
-
 def PRIMARY(tokens, idx):
-    if tokens[idx].type in ["verdaderoToken", "falsoToken", "nuloToken", "esteToken", "floatToken", "integerToken",
-                            "stringToken", "idToken", "superToken"]:
+    if tokens[idx].type in ["verdaderoToken", "falsoToken", "nuloToken", "esteToken", "floatToken", "integerToken", "stringToken", "idToken", "superToken"]:
         idx += 1
     elif tokens[idx].type == "superToken":
         idx += 1
@@ -713,7 +669,6 @@ def PRIMARY(tokens, idx):
         return -1
     return idx
 
-
 def FUNCTION(tokens, idx):
     if tokens[idx].type == "idToken":
         idx += 1
@@ -734,7 +689,6 @@ def FUNCTION(tokens, idx):
         return -1
     return idx
 
-
 def FUNCTIONS(tokens, idx):
     if tokens[idx].type == "rcorchToken":
         return idx
@@ -742,12 +696,10 @@ def FUNCTIONS(tokens, idx):
     idx = FUNCTIONS(tokens, idx)
     return idx
 
-
 def PARAMETERS_OPC(tokens, idx):
     if tokens[idx].type != "rparenToken":
         idx = PARAMETERS(tokens, idx)
     return idx
-
 
 def PARAMETERS(tokens, idx):
     if tokens[idx].type == "idToken":
@@ -757,7 +709,6 @@ def PARAMETERS(tokens, idx):
         print("Error: Missing parameter name at position " + str(idx))
         return -1
     return idx
-
 
 def PARAMETERS_2(tokens, idx):
     if tokens[idx].type == "comaToken":
@@ -770,18 +721,15 @@ def PARAMETERS_2(tokens, idx):
             return -1
     return idx
 
-
 def ARGUMENTS_OPC(tokens, idx):
     if tokens[idx].type != "rparenToken":
         idx = ARGUMENTS(tokens, idx)
     return idx
 
-
 def ARGUMENTS(tokens, idx):
     idx = EXPRESSION(tokens, idx)
     idx = ARGUMENTS_2(tokens, idx)
     return idx
-
 
 def ARGUMENTS_2(tokens, idx):
     if tokens[idx].type == "comaToken":
@@ -790,14 +738,13 @@ def ARGUMENTS_2(tokens, idx):
         idx = ARGUMENTS_2(tokens, idx)
     return idx
 
-
 def read_file(filename):
     if filename == "none" or filename == None:
         print("Por favor, ingrese los argumentos a tokenizar: ")
         try:
             while True:
-                text = input()
-                tokens = tokenizar(text)
+                text=input()
+                tokens=tokenizar(text)  
                 result = PROGRAM(tokens)
                 print(tokens)
                 if result != -1:
@@ -807,18 +754,17 @@ def read_file(filename):
         except KeyboardInterrupt:
             # code to execute when the user presses Ctrl+C
             print("Ctrl+C pressed. Exiting.")
-            return 0
+            return 0        
     else:
         file = open(filename, 'r')
         contents = file.read()
-        tokens = tokenizar(contents)
+        tokens=tokenizar(contents)  
         print(tokens)
         result = PROGRAM(tokens)
         if result != -1:
             print("La entrada es válida")
         else:
             print("Error de sintaxis")
-
 
 parser = argparse.ArgumentParser()
 parser.add_argument('filename', type=str, help='The filename to read')
